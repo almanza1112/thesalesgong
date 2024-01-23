692b3ffb6c81ca5002967bb835692b4c33dba91f
almanza1112 <almanza1112@gmail.com>
2024-01-22 20:30:03 -0500

    create account updated

---
+++ lib/globals.dart
@@ -0,0 +1,2 @@
+// End point
+String END_POINT = "http://192.168.0.118:3000"; // for when local testing
--- lib/login/admin_signup/admin_signup_page.dart
+++ lib/login/admin_signup/admin_signup_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:thesalesgong/globals.dart' as globals;
 
 class AdminSignupPage extends StatefulWidget {
   const AdminSignupPage({Key? key}) : super(key: key);
@@ -15,6 +20,10 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
   final TextEditingController _confirmPasswordController =
       TextEditingController();
   final double formPadding = 24;
+  String? _emailErrorText = null;
+
+  bool isPasswordVisible = false;
+  bool isConfirmPasswordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -25,12 +34,6 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
         backgroundColor: Colors.white10,
         foregroundColor: Colors.grey[600],
         shadowColor: Colors.transparent,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/add_team_members');
-        },
-        child: const Icon(Icons.arrow_forward),
       ),
       body: SafeArea(
         child: Form(
@@ -47,9 +50,9 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
                       labelText: 'Name',
                       floatingLabelBehavior: FloatingLabelBehavior.always,
                       prefixIcon: const Icon(
-                              Icons.person,
-                              color: Colors.grey,
-                            ),
+                        Icons.person,
+                        color: Colors.grey,
+                      ),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(40))),
                   validator: (value) {
@@ -65,6 +68,7 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
                   decoration: InputDecoration(
                       labelText: 'Email',
                       floatingLabelBehavior: FloatingLabelBehavior.always,
+                      errorText: _emailErrorText,
                       prefixIcon: const Icon(
                         Icons.email,
                         color: Colors.grey,
@@ -81,6 +85,7 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
                 SizedBox(height: formPadding),
                 TextFormField(
                   controller: _passwordController,
+                  obscureText: isPasswordVisible,
                   decoration: InputDecoration(
                       labelText: 'Password',
                       floatingLabelBehavior: FloatingLabelBehavior.always,
@@ -88,9 +93,19 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
                         Icons.lock,
                         color: Colors.grey,
                       ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
+                        icon: Icon(isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        color: Colors.grey,
+                      ),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(40))),
-                  obscureText: true,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter a password';
@@ -101,11 +116,24 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
                 SizedBox(height: formPadding),
                 TextFormField(
                   controller: _confirmPasswordController,
+                  obscureText: isConfirmPasswordVisible,
                   decoration: InputDecoration(
                       labelText: 'Confrim Password',
                       floatingLabelBehavior: FloatingLabelBehavior.always,
                       prefixIcon: const Icon(
                         Icons.lock,
+                        color: Colors.grey,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isConfirmPasswordVisible =
+                                !isConfirmPasswordVisible;
+                          });
+                        },
+                        icon: Icon(isConfirmPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off),
                         color: Colors.grey,
                       ),
                       border: OutlineInputBorder(
@@ -119,12 +147,62 @@ class _AdminSignupPageState extends State<AdminSignupPage> {
                     }
                     return null;
                   },
-                ),                
+                ),
+                const Spacer(),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      shape: const StadiumBorder()),
+                  onPressed: createAccount,
+                  child: const Text("CREATE ACCOUNT"),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void createAccount() async {
+    setState(() {
+      _emailErrorText = null;
+    });
+    if (_formKey.currentState!.validate()) {
+      var body = {
+        "name": _nameController.text.trim(),
+        "email": _emailController.text.trim(),
+        "password": _passwordController.text,
+      };
+
+      http.Response response = await http
+          .post(Uri.parse("${globals.END_POINT}/sign_up/admin"), body: body);
+
+      if (response.statusCode == 201) {
+        // Successully created new user
+        Map<String, dynamic> data = jsonDecode(response.body);
+        if (data['result'] == 'success' && context.mounted) {
+          // Navigate to add team members page
+          Navigator.pushNamed(context, '/add_team_members');
+        }
+      } else if (response.statusCode == 409) {
+        // errorInfo.code : "auth/email-already-exists" <-- this is from server
+        // Error creating new user: The email address is already in use by another account.
+        Map<String, dynamic> data = jsonDecode(response.body);
+        if (data['result'] == 'failure') {
+          setState(() {
+            _emailErrorText = "Email is already in use";
+          });
+        }
+      } else {
+        // Error creating new user
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Error creating new user")));
+        }
+      }
+    }
   }
 }
