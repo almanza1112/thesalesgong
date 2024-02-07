d29373a88883dfab58ffc74fa2f46afdbf89aebe
almanza1112 <almanza1112@gmail.com>
2024-02-06 19:12:00 -0500

    updating email applied

--- lib/menu/email_page.dart
+++ lib/menu/email_page.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:thesalesgong/globals.dart' as globals;
 
 class EmailPage extends StatefulWidget {
   const EmailPage({Key? key}) : super(key: key);
@@ -9,39 +12,81 @@ class EmailPage extends StatefulWidget {
 }
 
 class _EmailPageState extends State<EmailPage> {
-  TextEditingController _emailController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _confirmEmailController = TextEditingController();
+
+  final storage = FlutterSecureStorage();
+
   bool _isLoading = false;
+  String? emailErrorText;
 
-  Future<void> _editEmail() async {
-    setState(() {
-      _isLoading = true;
-    });
+  Future<void> _changeEmail() async {
+    FocusManager.instance.primaryFocus?.unfocus();
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
 
-    try {
-      // Make the HTTP POST request to edit the email content
-      final response = await http.post(
-        Uri.parse('https://example.com/edit-email'),
-        body: {
-          'email': _emailController.text,
-        },
-      );
+      var body = {
+        "uid": FirebaseAuth.instance.currentUser!.uid,
+        "old_email": FirebaseAuth.instance.currentUser!.email!,
+        "new_email": _emailController.text.trim(),
+        "team_ID": await storage.read(key: globals.FSS_TEAM_ID),
+      };
 
-      // Check the response status
-      if (response.statusCode == 200) {
-        // Email edited successfully
-        // TODO: Handle the response data
-      } else {
-        // Error occurred while editing email
+      try {
+        // Make the HTTP POST request to edit the email content
+        http.Response response = await http.post(
+            Uri.parse("${globals.END_POINT}/account/change_email"),
+            body: body);
+
+        // Check the response status
+        if (response.statusCode == 201) {
+          FirebaseAuth.instance.currentUser!.reload();
+          // Email edited successfully
+          setState(() {
+            _confirmEmailController.clear();
+          });
+
+          print(FirebaseAuth.instance.currentUser!.email);
+
+          if (context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Email edited successfully')),
+            );
+          }
+        } else if (response.statusCode == 409) {
+          // Email is already being used. Please try another email
+          setState(() {
+            emailErrorText =
+                "Email is already being used. Please try another email";
+          });
+        } else {
+          if (context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                  content:
+                      Text('There is an unepected error. Please try again.')),
+            );
+          }
+        }
+      } catch (e) {
+        // Error occurred while making the HTTP request
         // TODO: Handle the error
       }
-    } catch (e) {
-      // Error occurred while making the HTTP request
-      // TODO: Handle the error
-    }
 
-    setState(() {
-      _isLoading = false;
-    });
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _emailController.text = FirebaseAuth.instance.currentUser!.email!;
+    super.initState();
   }
 
   @override
@@ -50,24 +95,78 @@ class _EmailPageState extends State<EmailPage> {
       appBar: AppBar(
         title: const Text('Email'),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 24),
+                const Text(
+                  "You will be logged out after changing your email and must log back in with your new email.",
+                  style: TextStyle(fontSize: 14),
+                ),
+                const SizedBox(height: 24),
+                TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Email',
+                        errorText: emailErrorText,
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        prefixIcon: const Icon(
+                          Icons.email,
+                          color: Colors.grey,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter valid email';
+                      }
+                      return null;
+                    }),
+                const SizedBox(height: 16),
+                TextFormField(
+                    controller: _confirmEmailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Confirm New Email',
+                        errorText: emailErrorText,
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        prefixIcon: const Icon(
+                          Icons.email,
+                          color: Colors.grey,
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please confirm your new email';
+                      }
+                      if (value.trim() != _emailController.text.trim()) {
+                        return 'Emails do not match';
+                      }
+                      return null;
+                    }),
+                const Spacer(),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      shape: const StadiumBorder()),
+                  onPressed: _changeEmail,
+                  child: _isLoading
+                      ? const CircularProgressIndicator()
+                      : const Text("CHANGE EMAIL"),
+                ),
+                const SizedBox(height: 8),
+              ],
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _isLoading ? null : _editEmail,
-              child: _isLoading
-                  ? const CircularProgressIndicator()
-                  : const Text('Edit Email'),
-            ),
-          ],
+          ),
         ),
       ),
     );
